53423d7016fc67494f73ff7c284a8a5fb7a02dbf
Andrey <andrey1232145@gmail.com>
2023-03-13 23:41:49 +0300

    add 02 and fix bag in 01
    Errors in task 01 have been fixed and a small optimization has been added. Task 02 has been done.

--- 02/gen.sh
+++ 02/gen.sh
@@ -19,7 +19,7 @@ then
 elif [[ $size_sim -eq 2 ]]; 
 then
     count_sim_1=2
-    count_sim_2=2
+    count_sim_2=3
     for (( i=1; i<$caunter_gen; i++ )) do
         if [ $count_sim_1 -gt $count_sim_2 ];
         then
@@ -40,6 +40,7 @@ then
 elif [[ $size_sim -eq 3 ]];
 then
     count_sim_1=2
+    count_sim_3=2
     for (( i=1; i<$caunter_gen; i++ )) do
         if [ $count_sim_2 -gt $count_sim_3 ];
         then
@@ -65,13 +66,45 @@ then
             name+="${name_sim:2:1}"
         fi
     done
+elif [[ $size_sim -eq 4 ]];
+then
+    count_sim_1=2
+    for (( i=1; i<$caunter_gen; i++ )) do
+        if [ $count_sim_3 -gt $count_sim_4 ];
+        then
+            let count_sim_4++
+            count_sim_3=1
+            count_sim_2=1
+            count_sim_1=1
+        elif [ $count_sim_2 -gt $count_sim_3 ];
+        then
+            let count_sim_3++
+            count_sim_2=1
+            count_sim_1=1
+        elif [ $count_sim_1 -gt $count_sim_2 ];
+        then
+            let count_sim_2++
+            count_sim_1=1
+        else
+            let count_sim_1++
+        fi
+    done
+    for (( i=0; i<$( expr $count_sim_1 + $count_sim_2 + $count_sim_3 + $count_sim_4 ); i++ )) do
+        if [ $i -lt $count_sim_1 ];
+        then
+            name+="${name_sim:0:1}"
+        elif [ $i -lt $( expr $count_sim_1 + $count_sim_2 ) ];
+        then
+            name+="${name_sim:1:1}"
+        elif [ $i -lt $( expr $count_sim_1 + $count_sim_2 + $count_sim_3 ) ];
+        then
+            name+="${name_sim:2:1}"
+        else
+            name+="${name_sim:3:1}"
+        fi
+    done
 else
-    if [ $size_sim -eq 4 ]
-    then
-        count_sim_5=0
-        count_sim_6=0
-        count_sim_7=0
-    elif [ $size_sim -eq 5 ]
+    if [ $size_sim -eq 5 ]
     then
         count_sim_6=0
         count_sim_7=0
@@ -97,7 +130,7 @@ else
             count_sim_3=1
             count_sim_2=1
             count_sim_1=1
-        elif [ $size_sim -gt 4 ] && [ $count_sim_4 -gt $count_sim_5 ];
+        elif [ $count_sim_4 -gt $count_sim_5 ];
         then
             let count_sim_5++
             count_sim_4=1
@@ -123,7 +156,7 @@ else
             let count_sim_1++
         fi
     done
-    for (( i=0; i<$( expr $count_sim_1 + $count_sim_2 + $count_sim_3 + $count_sim_4 + $count_sim_5 + $count_sim_6 + $count_sim_7 ); i++ )) do
+    for (( i=0; i<$( expr $count_sim_1 + $count_sim_2 + $count_sim_3 + $count_sim_4 + $count_sim_5 + $count_sim_6 + $count_sim_7); i++ )) do
         if [ $i -lt $count_sim_1 ];
         then
             name+="${name_sim:0:1}"
@@ -136,12 +169,12 @@ else
         elif [ $i -lt $( expr $count_sim_1 + $count_sim_2 + $count_sim_3 + $count_sim_4 ) ];
         then
             name+="${name_sim:3:1}"
-        elif [ $size_sim -gt 4 ] && [ $i -lt $( expr $count_sim_1 + $count_sim_2 + $count_sim_3 \
-             + $count_sim_4 + $count_sim_5 ) ];
+        elif [ $i -lt $( expr $count_sim_1 + $count_sim_2 \
+             + $count_sim_3 + $count_sim_4 + $count_sim_5 ) ];
         then
             name+="${name_sim:4:1}"
-        elif [ $size_sim -gt 5 ] && [ $i -lt $( expr $count_sim_1 + $count_sim_2 + $count_sim_3 \
-             + $count_sim_4 + $count_sim_5 + $count_sim_6 ) ];
+        elif [ $size_sim -gt 5 ] && [ $i -lt $( expr $count_sim_1 + $count_sim_2 \
+             + $count_sim_3 + $count_sim_4 + $count_sim_5 + $count_sim_6 ) ];
         then
             name+="${name_sim:5:1}"
         elif [ $size_sim -eq 7 ];
@@ -151,4 +184,3 @@ else
     done
 fi
 echo $name
-
--- 02/main.sh
+++ 02/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 6 ]; then
+if [ "$#" -ne 3 ]; then
     echo "ERROR: The number of arguments should be 6 "
     exit 1
 fi
@@ -11,33 +11,13 @@ if [ $avail_size -le 1048576 ]; then
     exit 1
 fi
 
+START_TIME_NANOSEC=$(date +%s%N)
+START_TIME=$(date +%H:%M:%S)
 error_target=0
 NUMBER='[^0-9]+'
 ALPHABET='[^a-zA-Z]+'
-path=$1
-if [[ ${1:$((${#1}-1)):1} != / ]]; 
-then
-    echo "ERROR: The path must end with a '/' character "
-    error_target=1
-elif [ ! -d $path ];
-then
-    echo "ERROR: Folder $path doesn't exsist "
-    error_target=1
-fi
 
-number_of_subfolders=$2
-if [[ $number_of_subfolders =~ $NUMBER ]]; 
-then
-    if [[ $error_target == 1 ]];
-    then
-        echo
-    fi
-    echo "ERROR: The number of subfolders can only be described by a number "
-    echo "       Erroneous argument №2: $number_of_subfolders "
-    error_target=1
-fi
-
-character_for_name_dir=$3
+character_for_name_dir=$1
 no_repeat_cheker_dir=$(echo $character_for_name_dir | /bin/sed 's/\(.\)\1/\1/g')
 if [ ${#character_for_name_dir} -gt 7 ]
 then
@@ -46,7 +26,7 @@ then
         echo
     fi
     echo "ERROR: The number of characters to generate a folder name should not exceed 7 "
-    echo "       Erroneous argument №3: $character_for_name_dir "
+    echo "       Erroneous argument №1: $character_for_name_dir "
     error_target=1
 elif [[ $character_for_name_dir =~ $ALPHABET ]]
 then
@@ -55,7 +35,7 @@ then
         echo
     fi
     echo "ERROR: Only Latin letters should be used to generate the folder name "
-    echo "       Erroneous argument №3: $character_for_name_dir "
+    echo "       Erroneous argument №1: $character_for_name_dir "
     error_target=1
 elif [[ ${#no_repeat_cheker_dir} -ne ${#character_for_name_dir} ]]
 then
@@ -64,23 +44,11 @@ then
         echo
     fi
     echo "ERROR: The letters for generating folder names should not be repeated "
-    echo "       Erroneous argument №3: $character_for_name_dir "
+    echo "       Erroneous argument №1: $character_for_name_dir "
     error_target=1
 fi
 
-number_of_file=$4
-if [[ $number_of_file =~ $NUMBER ]]; 
-then
-    if [[ $error_target == 1 ]];
-    then
-        echo
-    fi
-    echo "ERROR: The number of subfolders can only be described by a number "
-    echo "       Erroneous argument №4: $number_of_file "
-    error_target=1
-fi
-
-character_for_file=$5
+character_for_file=$2
 character_for_name_file=${character_for_file%.*}
 character_for_expansion_file=${character_for_file#*.}
 no_repeat_cheker_file_name=$(echo $character_for_name_file | /bin/sed 's/\(.\)\1/\1/g')
@@ -92,7 +60,7 @@ then
         echo
     fi
     echo "ERROR: There should be a dot between the file name and the extension "
-    echo "       Erroneous argument №5: $character_for_file "
+    echo "       Erroneous argument №2: $character_for_file "
     error_target=1
 else
     if [ ${#character_for_name_file} -gt 7 ]
@@ -102,7 +70,7 @@ else
             echo
         fi
         echo "ERROR: The number of characters to generate a file name should not exceed 7 "
-        echo "       Erroneous argument №5: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
         error_target=1
     elif [[ $character_for_name_file =~ $ALPHABET ]]
     then
@@ -111,7 +79,7 @@ else
             echo
         fi
         echo "ERROR: Only Latin letters should be used to generate the file name "
-        echo "       Erroneous argument №5: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
         error_target=1
     elif [[ ${#no_repeat_cheker_file_name} -ne ${#character_for_name_file} ]]
     then
@@ -120,8 +88,8 @@ else
             echo
         fi
         echo "ERROR: The letters for generating file names should not be repeated "
-        echo "       Erroneous argument №5: $character_for_file "
-        echo "       Erroneous argument №5: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
         error_target=1
     fi
 
@@ -132,7 +100,7 @@ else
             echo
         fi
         echo "ERROR: The number of characters to generate a file expansion should not exceed 3 "
-        echo "       Erroneous argument №5: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
         error_target=1
     elif [[ $character_for_expansion_file =~ $ALPHABET ]]
     then
@@ -141,7 +109,7 @@ else
             echo
         fi
         echo "ERROR: Only Latin letters should be used to generate the file expansion "
-        echo "       Erroneous argument №5: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
         error_target=1
     elif [[ ${#no_repeat_cheker_file_expansion} -ne ${#character_for_expansion_file} ]]
     then
@@ -150,21 +118,21 @@ else
             echo
         fi
         echo "ERROR: The letters for generating file expansion should not be repeated "
-        echo "       Erroneous argument №5: $character_for_file "
+        echo "       Erroneous argument №2: $character_for_file "
         error_target=1
     fi
 fi
 
-size_file=$6
-size_num_files=${size_file%kb*}
-if [[ $size_num_files =~ $NUMBER || ! $size_file == *kb ]]; 
+size_file=$3
+size_num_files=${size_file%Mb*}
+if [[ $size_num_files =~ $NUMBER || ! $size_file == *Mb ]]; 
 then
     if [[ $error_target == 1 ]];
     then
         echo
     fi
-    echo "ERROR: The file size should be described by the number and signature kb after. "
-    echo "       Erroneous argument №6: $size_file "
+    echo "ERROR: The file size should be described by the number and signature Mb after. "
+    echo "       Erroneous argument №3: $size_file "
     error_target=1
 elif [ $size_num_files -gt 100 ]; 
 then
@@ -172,8 +140,8 @@ then
     then
         echo
     fi
-    echo "ERROR: The amount of memory for the file should not exceed 100 kb "
-    echo "       Erroneous argument №6: $size_file "
+    echo "ERROR: The amount of memory for the file should not exceed 100 Mb "
+    echo "       Erroneous argument №3: $size_file "
     error_target=1
 fi
 
@@ -181,15 +149,18 @@ if [[ $error_target != 1 ]];
 then
     /bin/touch "logger.log"
     data="_$(/bin/date +"%d%m%y")"
+    number_of_subfolders=$(($RANDOM%100))
     for (( i=1; i<=${number_of_subfolders}; i++ )) do
         avail_size=$(/bin/df -k / | /bin/grep /dev/mapper/ | /bin/awk '{print $4}')
         if [ $avail_size -le 1048576 ]; then
             echo "Error: Not enough memory"
             break
         fi
+        path="$(find /home -type d 2>/dev/null | head -n$(($RANDOM%650)) | tail -n1)/"
         name_dir="$(/bin/bash gen.sh $character_for_name_dir $i)"
         /bin/mkdir "${path}${name_dir}${data}"
         echo -e "${path}${name_dir}${data}/\t\t\t\t\t$(/bin/date +"%d.%m.%y")" >> logger.log
+        number_of_file=$(($RANDOM%100))
         for (( j=1; j<=${number_of_file}; j++ )) do
             avail_size=$(/bin/df -k / | /bin/grep /dev/mapper/ | /bin/awk '{print $4}')
             if [ $avail_size -le 1048576 ]; then
@@ -201,4 +172,12 @@ then
             echo -e "${path}${name_dir}${data}/${name_file}${data}.${character_for_expansion_file}\t$(/bin/date +"%d.%m.%y") ${size_num_files}" >> logger.log
         done
     done
+    END_TIME=$(date +%H:%M:%S)
+    END_TIME_NANOSEC=$(date +%s%N)
+    echo "Start time: $START_TIME"
+    echo "End time: $END_TIME"
+    echo "Total workong time: $((($END_TIME_NANOSEC - $START_TIME_NANOSEC)/1000000000)) seconds"
+    echo "Start time: $START_TIME" >> logger.log
+    echo "End time: $END_TIME" >> logger.log
+    echo "Total workong time: $((($END_TIME_NANOSEC - $START_TIME_NANOSEC)/1000000000)) seconds" >> logger.log
 fi
